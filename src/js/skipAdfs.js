b1a2498d71e22ace6b19436cf8965afe0612488c
Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
2022-04-30 11:31:19 +0900

    Modify sipAdfs.js

--- src/js/skipAdfs.js
+++ src/js/skipAdfs.js
@@ -1,10 +1,16 @@
 // Skip ADFS
-const skipADFS = () => {
-  if (document.domain === 'adfs.sic.shibaura-it.ac.jp') {
-    console.log('ADFSをスキップします')
-    document.getElementById('continuecontinueButton').click()
-    console.log('ADFSをスキップしました')
+export default () => {
+  const currentLocation = window.location.origin
+  if (currentLocation === 'https://adfs.sic.shibaura-it.ac.jp') {
+    window.addEventListener('DOMContentLoaded', skipADFS())
   } else {
     // Nothing
   }
+}
+
+const skipADFS = () => {
+  console.log('ADFSをスキップします')
+  const continueButton = document.getElementById('continueButton')
+  continueButton.click()
+  console.log('ADFSをスキップしました')
 }

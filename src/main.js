554ca558d6d70d6c518e797a0f0ac5a8e457ec29
Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
2022-04-30 16:11:13 +0900

    Setup rollup.js

--- src/main.js
+++ src/main.js
@@ -4,19 +4,6 @@ import modifySyllabus from './js/modifySyllabus'
 import skipLoginPage from './js/skipLoginPage'
 import skipADFS from './js/skipADFS'
 
-// ==UserScript==
-// @name         ScombZ-Utilities
-// @namespace    https://twitter.com/yudai1204
-// @version      2.5.2
-// @description  より快適なScombZライフのために、サイドメニュー、テスト、ログインを改善します
-// @author       @yudai1204 , @to_ku_me
-// @match        https://scombz.shibaura-it.ac.jp/*
-// @match        https://adfs.sic.shibaura-it.ac.jp/adfs/ls/*
-// @match        http://syllabus.sic.shibaura-it.ac.jp/*
-// @icon         https://scombz.shibaura-it.ac.jp/favicon.ico
-// @grant        none
-// ==/UserScript==
-
 const EXTENSION_VERSION = '2.5.2'
 console.log(`welcome to Scomb Utilities ver.${EXTENSION_VERSION}`)
 
